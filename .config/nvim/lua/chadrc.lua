49a4b342a9996a0ff112019a31ca13c0232e5040
travmonkey <travis@batk.me>
2024-04-27 00:42:54 -0500

    Add nvim

---
+++ .config/nvim/lua/chadrc.lua
@@ -0,0 +1,13 @@
+---@type ChadrcConfig
+local M = {}
+
+M.ui = {
+	theme = "catppuccin",
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+return M
---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre' -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   config = function()
+  --     require("nvchad.configs.lspconfig").defaults()
+  --     require "configs.lspconfig"
+  --   end,
+  -- },
+
+  -- {
+  -- 	"williamboman/mason.nvim",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"lua-language-server", "stylua",
+  -- 			"html-lsp", "css-lsp" , "prettier"
+  -- 		},
+  -- 	},
+  -- },
+  --
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
